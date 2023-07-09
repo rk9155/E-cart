91f5e28266f73cd847bf174429cfe865b9dc24ff
rk9155 <rakesh.kumar@314ecorp.com>
2023-07-09 15:13:12 +0530

    added more data

--- src/utils/restaurantData.js
+++ src/utils/restaurantData.js
@@ -1,14 +1,14 @@
 export const restaurantData = [
   {
     name: "Banana Muffin",
-    price: 250,
+    price: 150,
     id: "1",
     isVeg: "true",
     img: "https://res.cloudinary.com/hksqkdlah/image/upload/ar_1:1,c_fill,dpr_2.0,f_auto,fl_lossy.progressive.strip_profile,g_faces:auto,q_auto:low,w_344/SFS_Banana_Muffins_with_Sesame_and_Chocolate_Chunks_308_zessu8",
   },
   {
     name: "Blueberry Scone",
-    price: 305,
+    price: 205,
     id: "2",
     isVeg: "true",
     img: "https://www.starbucks.tt/wp-content/uploads/2017/05/blueberry-scone.jpg",
@@ -27,30 +27,43 @@ export const restaurantData = [
     img: "https://hips.hearstapps.com/delish/assets/16/07/1455633167-delish-copycat-starbucks-cookies-beauty.jpg",
   },
   {
-    name: "Banana Muffin",
-    price: 250,
+    name: "Round Chocolate cake",
+    price: 450,
     id: "5",
-    isVeg: "true",
-    img: "https://res.cloudinary.com/hksqkdlah/image/upload/ar_1:1,c_fill,dpr_2.0,f_auto,fl_lossy.progressive.strip_profile,g_faces:auto,q_auto:low,w_344/SFS_Banana_Muffins_with_Sesame_and_Chocolate_Chunks_308_zessu8",
+    img: "https://media.bakingo.com/sq-choco-truffle-cake0005choc-AA.jpg?tr=w-320,h-320,dpr-2,q-70",
   },
   {
-    name: "Blueberry Scone",
-    price: 305,
+    name: "Butterscotch Cake",
+    price: 480,
     id: "6",
-    isVeg: "true",
-    img: "https://www.starbucks.tt/wp-content/uploads/2017/05/blueberry-scone.jpg",
+    img: "https://media.bakingo.com/sq-butterscotch-cake0003butt-AA.jpg?tr=w-320,h-320,dpr-2,q-70",
   },
+
   {
-    name: "Carrot Cake",
-    price: 250,
+    name: "Half chocolate half Vanilla",
+    price: 450,
     id: "7",
-    isVeg: "true",
-    img: "https://cookingfrog.com/wp-content/uploads/2023/02/Starbucks-Carrot-Cake-Copycat-Recipe.jpg",
+    img: "https://media.bakingo.com/sq-choco-vanilla-cake0006chva-AA.jpg?tr=w-320,h-320,dpr-2,q-70",
   },
+
   {
-    name: "Home-Made Cookies",
-    price: 350,
+    name: "Fruit Cake",
+    price: 500,
     id: "8",
-    img: "https://hips.hearstapps.com/delish/assets/16/07/1455633167-delish-copycat-starbucks-cookies-beauty.jpg",
+    img: "https://media.bakingo.com/sq-fresh-fruit-cake0014frui-AA.jpg?tr=w-320,h-320,dpr-2,q-70",
+  },
+
+  {
+    name: "Chocochips and oreo jar",
+    price: 250,
+    id: "9",
+    img: "https://media.bakingo.com/sq-chocochips-oreo-jar-cake-jar249chocoreo-AA.jpg?tr=w-320,h-320,dpr-2,q-70",
+  },
+
+  {
+    name: "Kitkat",
+    price: 150,
+    id: "10",
+    img: "https://media.bakingo.com/scrumptious-kit-kat-pastries-past0211choc-AA.jpg?tr=w-320,h-320,dpr-2,q-70",
   },
 ];
